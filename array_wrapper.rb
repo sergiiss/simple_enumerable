cdfa4c9ea64f599cbcbb2f9f5fc83ca7f581f2a6
Sergiiss <Dracula23@mail.ru>
2017-09-19 06:46:04 +0300

    First commit

---
+++ array_wrapper.rb
@@ -0,0 +1,17 @@
+require_relative 'simple_enumerable'
+
+class ArrayWrapper
+  include SimpleEnumerable
+
+  def initialize(*items)
+    @items = items.flatten
+  end
+
+  def each(&block)
+    @items.each(&block)
+    self
+  end
+end
+
+foo = ArrayWrapper.new([5,6,7,8,9,10])
+puts foo.inject(1) {|memo, i| memo * i}
---
+++ simple_enumerable.rb
@@ -0,0 +1,50 @@
+module SimpleEnumerable
+  def map(&block)
+    result = []
+    each do |elem|
+      result << block.call(elem)
+    end
+
+    result
+  end
+
+  def find(ifnone = nil, &block)
+    result = nil
+    found = false
+    each do |elem|
+      if block.call(elem)
+        result = elem
+        found = true
+        break
+      end
+    end
+
+    found ? result : ifnone && ifnone.call
+  end
+
+  def reject(&block)
+    result = []
+    each do |elem|
+      result << elem unless block.call(elem)
+    end
+
+    result
+  end
+
+  def select(&block)
+    result = []
+    each do |elem|
+      result << elem if block.call(elem)
+    end
+
+    result
+  end
+
+  def inject(accumulator = 0, &block)
+    each do |elem|
+      accumulator = block.call(accumulator, elem)
+    end
+
+    accumulator
+  end
+end
